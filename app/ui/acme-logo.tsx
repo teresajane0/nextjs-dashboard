6817beeb511278bbd7623f27e50210ce8652f3b1
Teresa Jane Sotto <sottoteresajane@gmail.com>
2025-09-16 19:25:33 +0800

    Added Image

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,7 +1,7 @@
+import { lusitana } from '@/app/ui/Font';
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
-
 export default function AcmeLogo() {
+
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
