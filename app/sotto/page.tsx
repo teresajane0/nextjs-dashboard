68f3f6c98236a5bd024269b1474acc37d765ac02
Teresa Jane Sotto <sottoteresajane@gmail.com>
2025-10-06 18:05:58 +0800

    added navigation

--- app/sotto/page.tsx
+++ app/sotto/page.tsx
@@ -1,9 +1,11 @@
+import Info from "./info"
 export default function Page() {
+    
     return(
         <div>
-        <h1 className="text-3x1 font-bold">Jane</h1>
-<info />
+        <h1 className="text-3x1 font-bold">TERESA JANE</h1>
+<Info/>
         </div>
-    ) ;
+    ) 
 
 }
